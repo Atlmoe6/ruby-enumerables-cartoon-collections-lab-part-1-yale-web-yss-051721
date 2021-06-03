64c424fdc87e12c145eafaddceb86a1dec96c687
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-03 17:32:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,20 @@
+require 'pry'
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+    
+  characters_array = ["Dopey", "Grumpy", "Bashful"]
+  characters_array.each do |character_array|
+    puts "Hello #{character_array}!"
+
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  
+  
+  
 end
